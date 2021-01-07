c1faa36ae27b8c8ae99a58a856209c4740512c0b
bayele250 <45609969+bayele250@users.noreply.github.com>
2021-01-07 01:25:16 -0500

    Adding swagger
    Adding swagger

--- CarComparisonTool/CarComparisonTool/Startup.cs
+++ CarComparisonTool/CarComparisonTool/Startup.cs
@@ -33,11 +33,22 @@ namespace CarComparisonTool
             services.AddScoped<ICarService, CarService> ();
             services.AddSingleton<ICarData, CarData>();
             services.AddAutoMapper(typeof(AutoMappperHelper));
+            services.AddSwaggerGen( c=>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title ="Cars Comparison API", Version ="v1"});
+            }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cars Comparison API");
+            }
+            );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
